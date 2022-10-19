30192660a52b593da39b1cd46569567b3f31084e
MuhireIghor <muhireighor123@gmail.com>
2022-10-19 12:58:13 +0200

    List item componet with redirection by id

--- components/ArticleItem.js
+++ components/ArticleItem.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+import articleStyles from '../styles/Article.module.css';
+import Link from 'next/link';
 
-const ArticleItem = () => {
+const ArticleItem = ({article}) => {
   return (
-    <div>ArticleItem</div>
+    <div className={articleStyles.card}>
+        <Link href='/articles/[id]' as={`/articles/${article.id}`}>
+            <a>
+                {article.title}&rarr;
+            </a>
+
+        </Link>
+        <h3>{article.body}</h3>
+
+    </div>
   )
 }
 
--- components/ArticleList.js
+++ components/ArticleList.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import articleStyles from '../styles/Article.module.css';
+import ArticleItem from './ArticleItem';
 
 const ArticleList = ({articles,index}) => {
   return (
     <div className={articleStyles.grid}>
    {articles.map((article)=>(
-  <h3 key={index}>{article.title}</h3>
+<ArticleItem article={article} />
 
 
 ))}
